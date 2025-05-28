1af0ac523babe079e56ebe577862fcf97096d7b9
Andrew Knight <andrew.knight14wfc@gmail.com>
2025-05-28 10:06:54 +0100

    modified film classficiation tests

--- code-examples/filmClassification/main_test.go
+++ code-examples/filmClassification/main_test.go
@@ -3,10 +3,11 @@ package main
 import "testing"
 
 func TestGetClassificationByAge(t *testing.T) {
-	type args struct {
+	//args is scalable - helps if function has multiple input parameters - easier to organise
+	type args struct { //makes test cases
 		age int
 	}
-	tests := []struct {
+	tests := []struct { //slice of anonymous structs, where each represents a test case
 		name string
 		args args
 		want string
@@ -18,7 +19,8 @@ func TestGetClassificationByAge(t *testing.T) {
 		{"age17-U+PG+12+15", args{17}, "U, PG, 12 & 15 films are available"},
 		{"age18-All", args{18}, "All films are available"},
 	}
-	for _, tt := range tests {
+	//tt is current test case, _ index so is unused
+	for _, tt := range tests { //runs test cases - runs each element in tests
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GetAgeClassification(tt.args.age); got != tt.want {
 				t.Errorf("GetAgeClassification() = %v, want %v", got, tt.want)
